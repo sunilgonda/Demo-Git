1fb07f3c6f08d6f9ae94749b568a36e2b7389d8f
sunil gonda <sunilgonda7@gmail.com>
2022-06-24 12:55:23 +0530

    first commit

---
+++ DemoGit.java
@@ -0,0 +1,10 @@
+package Git;
+
+public class DemoGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello git");
+	}
+
+}
---
+++ demotest.java
@@ -0,0 +1,10 @@
+package Git;
+
+public class demotest {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+		System.out.println("new test");
+	}
+
+}
